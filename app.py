beb58cb167491ea7f9404db39126ca44584172ab
Aquaduckd <aquaduckd@gmail.com>
2025-06-16 19:23:34 -0400

    move logic into main

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ import discord
 from discord import Message
 from importlib import import_module
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-bot = discord.Client(intents=intents)
-
 class Command:
     def __init__(self, func: any, use: str, desc: str):
         self.func = func
@@ -20,7 +15,7 @@ class Command:
 class App:
     def __init__(self):
         self.reload_commands()
-
+    
     async def run(self, command: str, message: Message):
         await message.channel.send(self.commands[command].run(message), reference=message)
 
@@ -39,29 +34,34 @@ class App:
                     mod.desc() if hasattr(mod, "desc") else None,
                 )
 
-app = App()
-
-@bot.event
-async def on_message(message: Message):
-    if message.author.bot or not message.content:
-        return
-    
-    args = message.content.split()
-    if isinstance(message.channel, discord.channel.DMChannel):
-        command = args[0].lower()
-    elif args[0] in "!amini !bmini !cmini !dvormini !cnini".split():
-        command = args[1].lower() if len(args) > 1 else None
-    else:
-        return
-    
-    if command in app.commands:
-        await app.run(command, message)
-    elif command:
-        await message.channel.send(f"Error: {command} is not an available command", reference=message)
-    else:
-        await message.channel.send("Try `!cmini help`", reference=message)
-
 def main():
+    app = App()
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+    bot = discord.Client(intents=intents)
+
+    @bot.event
+    async def on_message(message: Message):
+        if message.author.bot or not message.content:
+            return
+        
+        args = message.content.split()
+        if isinstance(message.channel, discord.channel.DMChannel):
+            command = args[0].lower()
+        elif args[0] in "!amini !bmini !cmini !dvormini !cnini".split():
+            command = args[1].lower() if len(args) > 1 else None
+        else:
+            return
+        
+        if command in app.commands:
+            await app.run(command, message)
+        elif command:
+            await message.channel.send(f"Error: {command} is not an available command", reference=message)
+        else:
+            await message.channel.send("Try `!cmini help`", reference=message)
+    
     bot.run(open("token.txt", "r").read())
 
 if __name__ == "__main__":
